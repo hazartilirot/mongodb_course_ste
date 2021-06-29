c5c388c1baf203f7120b7c78a85a171937b3955b
hazartilirot <hazartilirot@gmail.com>
2021-06-29 15:59:05 +0300

    test checks if a new record with an invalid name is prevented from being saved

--- test/validation_test.mjs
+++ test/validation_test.mjs
@@ -9,10 +9,21 @@ describe('Validating records', () => {
     const { message } = validationResult.errors.name;
     assert(message === 'Name is required');
   });
+
   it('requires a user\'s name longer than 2 character', () => {
     const user = new User({ name: 'Ka' });
     const validationResult = user.validateSync();
     const { message } = validationResult.errors.name;
     assert(message === 'The name must be longer than 2 characters');
+  });
+  
+  it('prevents a new record from being saved', done => {
+    const user = new User({ name: 'El' });
+    user.save()
+      .catch(validationResult => {
+        const { message } = validationResult.errors.name;
+        assert(message, 'The name must be longer than 2 characters');
+        done();
+      });
   });
 });
